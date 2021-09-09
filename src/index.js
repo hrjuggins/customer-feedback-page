1b1103e6a84f6ef97ac75ef5e223b8ea93b302fc
hrjuggins <hrjuggins@gmail.com>
2021-09-09 16:27:13 +0100

    Revert "Setup"
    This reverts commit f3a02bf5eff80bbbc771970b4acd3dd4807d506a.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 import "../sass/layout.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
 
 module.hot.accept();
